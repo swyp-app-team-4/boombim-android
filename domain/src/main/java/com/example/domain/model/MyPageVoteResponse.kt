bc5db9a68b25a80a1f0cfa63f625ce840385c110
park-hwaan <parkhwan1129@gmail.com>
2025-08-27 16:40:35 +0900

    design: 마이페이지 나의 투표,질문 디자인 변경

--- domain/src/main/java/com/example/domain/model/MyPageVoteResponse.kt
+++ domain/src/main/java/com/example/domain/model/MyPageVoteResponse.kt
@@ -4,15 +4,21 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class MyPageVoteResponse(
-    val day: String, // 날짜 (예: "2025-08-19T00:00:00")
+    val day: String, // 날짜 (예: "2025-08-26T12:39:48.199Z")
     val res: List<PopularityDetail>
 )
 
 @Serializable
 data class PopularityDetail(
     val voteId: Int,
-    val day: String, // 투표 시간 (예: "2025-08-19T16:31:06.979297")
-    val posName: String, // 위치 이름 (예: "구로역")
-    val popularStatus: String, // 혼잡도 상태 (예: "RELAXED")
-    val popularCnt: Int // 투표 수
+    val profile: List<String>, // 프로필 이미지 URL
+    val day: String, // 투표 시간 (예: "2025-08-26T12:39:48.201Z")
+    val posName: String, // 위치 이름 (예: "강남 교보문고")
+    val popularRes: List<String>, // 혼잡도 상태 (예: "CROWED")
+    val relaxedCnt: Int,
+    val commonly: Int,
+    val slightlyBusyCnt: Int,
+    val crowedCnt: Int,
+    val voteAllCnt: Int,
+    val voteStatus: String
 )
